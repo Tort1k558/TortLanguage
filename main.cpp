4f7f2e262853dd968762933fc9eba99053da3a59
Tort1k <lolododo558@gmail.com>
2023-05-13 01:27:38 +0400

    Update README.md

--- main.cpp
+++ main.cpp
@@ -13,31 +13,27 @@
 int main() {
     std::string m_input =
         R"(
-        def pow(int a,int b)
-        {
-            print a;
-            print b;
-            return a*b;
-        }
         def sum(int a,int b)
         {
             return a+b;
         }
+    
         def double div(double a,double b);
 
         def main()
         {
             int a = 10,b =5;
-            b = sum(5,8);
-            int d = a || b;
-            int c = b > a;
-            a = 25**4;
             print a;
-            return a;
+            print b;
+            b = sum(5,8);
+            print a + b * 10;
+            double c = div(5.0,2.0);
+            print c;
+            return c;
         }
         def div(double a,double b)
         {
-            return a/b;
+            return a / b ;
         })";
 
     auto lexer = std::make_shared<Lexer>(m_input);
